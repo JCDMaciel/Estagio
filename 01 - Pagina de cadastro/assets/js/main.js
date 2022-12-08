687ee620c45b1b83c17a772f811846aad24d197d
JCDMaciel <tyupoi49@gmail.com>
2022-12-07 21:30:01 -0300

    consertando required e regex

--- 01 - Pagina de cadastro/assets/js/main.js
+++ 01 - Pagina de cadastro/assets/js/main.js
@@ -1,58 +1,56 @@
 const tabela = document.getElementById('tabela');
 
 function criarLinhaDaTabela () {
-    const varNome = document.getElementById('nome').value;
-    const varCpf = document.getElementById('cpf').value;
-    const varEmail = document.getElementById('email').value;
-    const varEndereco = document.getElementById('endereco').value;
-    const varNascimento = document.getElementById('nascimento').value;
-    const varPrimeiroContato = document.getElementById('primeiro_contato').value;
-    const varSegundoContato = document.getElementById('segundo_contato').value;
+    const campoNome = document.getElementById('nome').value;
+    const campoCpf = document.getElementById('cpf').value;
+    const campoEmail = document.getElementById('email').value;
+    const campoEndereco = document.getElementById('endereco').value;
+    const campoNascimento = document.getElementById('nascimento').value;
 
     const novaLinha = document.createElement("tr");
     novaLinha.setAttribute("class", "linha");
 
     const novoNome = document.createElement("td");
     novaLinha.appendChild(novoNome);
-    novoNome.innerHTML = varNome;
+    novoNome.innerHTML = campoNome;
     novoNome.setAttribute("class", "tabela-item");
 
     const novoCpf = document.createElement("td");
     novaLinha.appendChild(novoCpf);
-    novoCpf.innerHTML = varCpf;
+    novoCpf.innerHTML = campoCpf;
     novoCpf.setAttribute("class", "tabela-item");
 
     const novoEmail = document.createElement("td");
     novaLinha.appendChild(novoEmail);
-    novoEmail.innerHTML = varEmail;
+    novoEmail.innerHTML = campoEmail;
     novoEmail.setAttribute("class", "tabela-item");
 
     const novoEndereco = document.createElement("td");
     novaLinha.appendChild(novoEndereco);
-    novoEndereco.innerHTML = varEndereco;
+    novoEndereco.innerHTML = campoEndereco;
     novoEndereco.setAttribute("class", "tabela-item");
 
     const novoNascimento = document.createElement("td");
     novaLinha.appendChild(novoNascimento);
-    novoNascimento.innerHTML = varNascimento;
+    novoNascimento.innerHTML = campoNascimento;
     novoNascimento.setAttribute("class", "tabela-item");
 
-    const novoPrimeiroContato = document.createElement("td");
-    novaLinha.appendChild(novoPrimeiroContato);
-    novoPrimeiroContato.innerHTML = varPrimeiroContato;
-    novoPrimeiroContato.setAttribute("class", "tabela-item");
+    const botaoExcluir = document.createElement("input");
+    novaLinha.appendChild(botaoExcluir);
+    botaoExcluir.setAttribute("onclick", "removerUltimaLinhaDaTabela ();");
+    botaoExcluir.setAttribute("id", "apagar");
+    botaoExcluir.setAttribute("value", "excluir");
+    botaoExcluir.style.width = "2.5rem";
 
-    const novoSegundoContato = document.createElement("td");
-    novaLinha.appendChild(novoSegundoContato);
-    novoSegundoContato.innerHTML = varSegundoContato;
-    novoSegundoContato.setAttribute("class", "tabela-item");
+    const botaoEditar = document.createElement("input");
+    novaLinha.appendChild(botaoEditar);
+    botaoEditar.setAttribute("onclick", "editarUltimaLinhaDaTabela ();");
+    botaoEditar.setAttribute("id", "editar");
+    botaoEditar.setAttribute("value", "editar");
+    botaoEditar.style.width = "2.5rem";
 
     tabela.appendChild(novaLinha);
 }
-
-cadastrar.addEventListener('click', ()=>{
-    criarLinhaDaTabela ();
-})
 
 function removerUltimaLinhaDaTabela () {
     const linhas = document.getElementsByClassName("linha");
@@ -60,6 +58,12 @@ function removerUltimaLinhaDaTabela () {
     tabela.removeChild(ultimaLinha);
 }
 
-apagar.addEventListener('click', ()=>{
+function editarUltimaLinhaDaTabela () {
     removerUltimaLinhaDaTabela ();
-})
+ 
+    campoNome.value = document.getElementById('nome').value;
+    campoCpf.value = document.getElementById('cpf').value;
+    campoEmail.value = document.getElementById('email').value;
+    campoEndereco.value = document.getElementById('endereco').value;
+    campoNascimento.value = document.getElementById('nascimento').value;
+}
